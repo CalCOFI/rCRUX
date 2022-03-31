2f48eaa887c6d24b8cb37d863d66c8c5dc39a040
Luna <monstersmakemehorny@gmail.com>
2022-03-31 18:00:14 -0400

    Did some testing

--- scripts/example.R
+++ scripts/example.R
@@ -3,10 +3,10 @@
 blast_seeds_parent <- "D:/blast_seeds"
 accession_taxa_path <- "D:/accessionTaxa.sql"
 
-get_blast_seeds("TAGAACAGGCTCCTCTAG", "TTAGATACCCCACTATGC",
-                blast_seeds_parent, "12S_V5F1", accession_taxa_path,
-                organism = c("7776"), MAX_TARGET_PER_TEMPLATE = 10, return_table = FALSE)
+test1 <- get_blast_seeds("TAGAACAGGCTCCTCTAG", "TTAGATACCCCACTATGC",
+                blast_seeds_parent, "12S_V5F1_1", accession_taxa_path,
+                organism = c("7776"), return_table = FALSE)
 
-get_blast_seeds("TAGAACAGGCTCCTCTAG", "TTAGATACCCCACTATGC",
-                blast_seeds_parent, "12S_V5F1", accession_taxa_path,
+test2 <- get_blast_seeds("TAGAACAGGCTCCTCTAG", "TTAGATACCCCACTATGC",
+                blast_seeds_parent, "12S_V5F1_2", accession_taxa_path,
                 organism = c("7776", "7777"), return_table = FALSE)
--- scripts/functions.R
+++ scripts/functions.R
@@ -146,6 +146,7 @@ get_blast_seeds <- function(forward_primer, reverse_primer,
   
   #return if you're supposed to
   if(return_table) {
+    print("dogs")
     return(to_be_blasted_entries)
   }
 }
