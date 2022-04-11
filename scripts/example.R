be826ed166fc53c206959a3cfa708c6566e16e82
Luna <monstersmakemehorny@gmail.com>
2022-04-11 13:19:11 -0400

    Made the error handling actually work

--- scripts/example.R
+++ scripts/example.R
@@ -12,7 +12,4 @@ test1 <- get_blast_seeds("TAGAACAGGCTCCTCTAG", "TTAGATACCCCACTATGC",
 
 test2 <- get_blast_seeds("TAGAACAGGCTCCTCTAG", "TTAGATACCCCACTATGC",
                          blast_seeds_parent, "12S_V5F1_1", accession_taxa_path,
-                         num_permutations = 25, hitsize='1000000', evalue='100000',
-                         word_size='6', MAX_TARGET_PER_TEMPLATE = '5000',
-                         NUM_TARGETS_WITH_PRIMERS ='500000',
                          organism = c("7776", "7777"), return_table = FALSE)
