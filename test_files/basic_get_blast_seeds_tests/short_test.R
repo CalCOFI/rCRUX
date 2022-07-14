2d1f1d97790df797378677d57ba160352cd3df19
Luna <monstersmakemehorny@gmail.com>
2022-07-14 15:03:36 -0400

    correction for real

--- test_files/basic_get_blast_seeds_tests/short_test.R
+++ test_files/basic_get_blast_seeds_tests/short_test.R
@@ -2,7 +2,7 @@
 sink("short_test.txt")
 accession_taxa_path <- "/data/home/galoscarleo/taxonomy/accessionTaxa.sql"
 
-get_blast_seeds("TAGAACAGGCTCCTCTAG", "TTAGATACCCCACTATGC", "",
+RCRUX.dev::get_blast_seeds("TAGAACAGGCTCCTCTAG", "TTAGATACCCCACTATGC", "",
                 "12S_V5F1", accession_taxa_path, organism = c("7776", "7777"),
                 return_table = FALSE)
 sink()
