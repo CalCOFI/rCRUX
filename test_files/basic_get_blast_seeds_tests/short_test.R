7e937c15802204add5cfd4de8611ade6c3c165cd
Luna <monstersmakemehorny@gmail.com>
2022-07-14 15:00:33 -0400

    added test file

---
+++ test_files/basic_get_blast_seeds_tests/short_test.R
@@ -0,0 +1,8 @@
+# Test the basic functionality of get_blast_seeds
+sink("short_test.txt")
+accession_taxa_path <- "/data/home/galoscarleo/taxonomy/accessionTaxa.sql"
+
+get_blast_seeds("TAGAACAGGCTCCTCTAG", "TTAGATACCCCACTATGC", "",
+                "12S_V5F1", accession_taxa_path, organism = c("7776", "7777"),
+                return_table = FALSE)
+sink()
