954ab935d1e0f5dfafe961ddf5edd76942ffe1c7
Luna <monstersmakemehorny@gmail.com>
2022-07-19 18:37:09 -0400

    modified test files

--- test_files/basic_get_blast_seeds_tests/short_test.R
+++ test_files/basic_get_blast_seeds_tests/short_test.R
@@ -2,15 +2,16 @@
 devtools::install_github("LunaGal/RCRUX.dev")
 
 accession_taxa_path <- "/data/home/galoscarleo/taxonomy/accessionTaxa.sql"
+db_path <- ""
 
 
 sink("short_test_out.txt")
 err <- file("short_test_err.txt", open = "w")
 sink(err, type = "message")
 tryCatch(
-    RCRUX.dev::get_blast_seeds("TAGAACAGGCTCCTCTAG", "TTAGATACCCCACTATGC", "",
-        "12S_V5F1", accession_taxa_path, organism = c("7776", "7777"),
-        return_table = FALSE),
+    RCRUX.dev::get_blast_seeds("TAGAACAGGCTCCTCTAG", "TTAGATACCCCACTATGC",
+        "short_test/", "12S_V5F1", accession_taxa_path,
+        organism = c("7776", "7777"), return_table = FALSE),
     error = function(e) {
         message(e, "\n")
     }
@@ -22,7 +23,7 @@ err <- file("large_test_err.txt", open = "w")
 sink(err, type = "message")
 tryCatch(
     RCRUX.dev::get_blast_seeds("TAGAACAGGCTCCTCTAG", "TTAGATACCCCACTATGC",
-                         "", "12S_V5F1", accession_taxa_path,
+                         "large_test/", "12S_V5F1", accession_taxa_path,
                          num_permutations = 25, hitsize = "1000000",
                          evalue = "100000", word_size = "6",
                          MAX_TARGET_PER_TEMPLATE = "5000",
@@ -40,13 +41,25 @@ sink(err, type = "message")
 tryCatch(
     RCRUX.dev::get_blast_seeds("GGWACWGGWTGAACWGTWTAYCCYCC",
                                 "TANACYTCnGGRTGNCCRAARAAYCA",
-                                blast_seeds_parent, "CO1_063022",
+                                "many_ns_test/", "CO1_063022",
                                 accession_taxa_path,
                                 num_permutations = 20, hitsize = "1000000",
                                 evalue = "100000", word_size = "6",
                                 MAX_TARGET_PER_TEMPLATE = "5",
                                 NUM_TARGETS_WITH_PRIMERS = "500000",
                                 organism = c("33208"), return_table = FALSE),
+    error = function(e) {
+        message(e, "\n")
+    }
+)
+closeAllConnections()
+
+sink("rcrux_blast_test_out.txt")
+err <- file("rcrux_blast_test_err.txt", open = "w")
+sink(err, type = "message")
+tryCatch(
+    RCRUX.dev::rcrux_blast("12S_V5F1/12S_V5F1_primerTree_output_with_taxonomy.csv",
+                            "blast_test_save", db_path, accession_taxa_path),
     error = function(e) {
         message(e, "\n")
     }
