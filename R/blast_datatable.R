8abd2d685255e089d60d21a8f0d155ebc8e58e54
limey-bean <eecurd@g.ucla.edu>
2022-09-15 10:15:57 -0400

    is check_db() the problem?

--- R/blast_datatable.R
+++ R/blast_datatable.R
@@ -60,10 +60,10 @@ blast_datatable <- function(blast_seeds, save_dir, db, accession_taxa_path,
                             ncbi_bin = NULL, force_db = FALSE,
                             sample_size = 1000, wildcards = "NNNN") {
 
-  if (!(check_db(db) || force_db)) {
-    stop(db, " is probably not a blast database.
-         Use force_db = TRUE to try it anyway.")
-  }
+  #if (!(check_db(db) || force_db)) {
+  #  stop(db, " is probably not a blast database.
+  #       Use force_db = TRUE to try it anyway.")
+  #}
 
   # Default values for tracker variables
   num_rounds <- 1
