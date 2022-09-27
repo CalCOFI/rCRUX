21a1f756183b7366223a1a444ae11ed76e880d73
limey-bean <eecurd@g.ucla.edu>
2022-09-27 09:23:26 -0400

    typo

--- R/blast_datatable.R
+++ R/blast_datatable.R
@@ -154,7 +154,7 @@ blast_datatable <- function(blast_seeds, save_dir, db, accession_taxa_path,
     message(paste(length(sample_indices), " indices to blast for ", rank, "
           during this round in subsets of ", max_to_blast, " or fewer" ))
 
-    if (length(sample_indices <= max_to_blast) {
+    if (length(sample_indices <= max_to_blast)) {
 
       run_blastdbcmd_blastn_and_aggregate_resuts(sample_indices,
                         blast_seeds_m, db, ncbi_bin = NULL, too_many_ns, db_dir,
