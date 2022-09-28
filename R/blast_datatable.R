896bb3c69bf94e7a69cb0a85c058ce10fa5d1eff
limey-bean <eecurd@g.ucla.edu>
2022-09-28 13:33:35 -0400

    Update blast_datatable.R

--- R/blast_datatable.R
+++ R/blast_datatable.R
@@ -183,7 +183,7 @@ blast_datatable <- function(blast_seeds, save_dir, db, accession_taxa_path,
     unsampled_indices <-
       unsampled_indices[!(unsampled_indices %in% sample_indices)]
 
-    save_state(save_dir, output_table, unsampled_indices, too_many_ns,
+    #save_state(save_dir, output_table, unsampled_indices, too_many_ns,
                   blastdbcmd_failed, num_rounds, blast_seeds_m)
 
 
@@ -200,27 +200,27 @@ blast_datatable <- function(blast_seeds, save_dir, db, accession_taxa_path,
       # Pick up where it left off
       if (file.exists(paste(save_dir, "unsampled_indices.txt", sep = "/"))) {
 
-        rounds_path <- paste(save_dir, "num_rounds.txt", sep = "/")
-        num_rounds <- as.numeric(readLines(con = rounds_path))
+      rounds_path <- paste(save_dir, "num_rounds.txt", sep = "/")
+      num_rounds <- as.numeric(readLines(con = rounds_path))
 
-        ns_path <- paste(save_dir, "too_many_ns.txt", sep = "/")
-        too_many_ns <- as.numeric(readLines(con = ns_path))
+      ns_path <- paste(save_dir, "too_many_ns.txt", sep = "/")
+      too_many_ns <- as.numeric(readLines(con = ns_path))
 
-        blastdbcmd_failed_path <- paste(save_dir, "blastdbcmd_failed.txt", sep = "/")
-        blastdbcmd_failed <- as.numeric(readLines(con = blastdbcmd_failed_path))
+     blastdbcmd_failed_path <- paste(save_dir, "blastdbcmd_failed.txt", sep = "/")
+      blastdbcmd_failed <- as.numeric(readLines(con = blastdbcmd_failed_path))
 
-        unsampled_indices_path <-
+      unsampled_indices_path <-
           paste(save_dir, "unsampled_indices.txt", sep = "/")
 
-        unsampled_indices <-
+      unsampled_indices <-
           as.numeric(readLines(con = unsampled_indices_path))
 
-        output_table_path <- paste(save_dir, "output_table.txt", sep = "/")
-        output_table <- read.csv(output_table_path, colClasses = "character")
+      output_table_path <- paste(save_dir, "output_table.txt", sep = "/")
+      output_table <- read.csv(output_table_path, colClasses = "character")
 
 
-        blast_seeds_m_path <- paste(save_dir, "blast_seeds_passed_filter.txt", sep = "/")
-        blast_seeds_m <- read.csv(blast_seeds_m_path, colClasses = "character")
+      blast_seeds_m_path <- paste(save_dir, "blast_seeds_passed_filter.txt", sep = "/")
+      blast_seeds_m <- read.csv(blast_seeds_m_path, colClasses = "character")
 
       }
 
