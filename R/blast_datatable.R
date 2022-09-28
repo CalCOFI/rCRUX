8fd7c0a80204df43c70f9de3fdd9514bcfacf425
limey-bean <eecurd@g.ucla.edu>
2022-09-28 13:29:15 -0400

    smarter not harder?

--- R/blast_datatable.R
+++ R/blast_datatable.R
@@ -80,54 +80,37 @@ blast_datatable <- function(blast_seeds, save_dir, db, accession_taxa_path,
   unsampled_indices <- seq_along(blast_seeds_m$accession)
 
 ######### testing
-  message("1")
+#  message("1")
 
   # Pick up where it left off
-  if (file.exists(paste(save_dir, "unsampled_indices.txt", sep = "/"))) {
+#  if (file.exists(paste(save_dir, "unsampled_indices.txt", sep = "/"))) {
 
-    rounds_path <- paste(save_dir, "num_rounds.txt", sep = "/")
-    num_rounds <- as.numeric(readLines(con = rounds_path))
+#    rounds_path <- paste(save_dir, "num_rounds.txt", sep = "/")
+#    num_rounds <- as.numeric(readLines(con = rounds_path))
 
-    ns_path <- paste(save_dir, "too_many_ns.txt", sep = "/")
-    too_many_ns <- as.numeric(readLines(con = ns_path))
+#    ns_path <- paste(save_dir, "too_many_ns.txt", sep = "/")
+#    too_many_ns <- as.numeric(readLines(con = ns_path))
 
-    blastdbcmd_failed_path <- paste(save_dir, "blastdbcmd_failed.txt", sep = "/")
-    blastdbcmd_failed <- as.numeric(readLines(con = blastdbcmd_failed_path))
+#    blastdbcmd_failed_path <- paste(save_dir, "blastdbcmd_failed.txt", sep = "/")
+#    blastdbcmd_failed <- as.numeric(readLines(con = blastdbcmd_failed_path))
 
-    unsampled_indices_path <-
-      paste(save_dir, "unsampled_indices.txt", sep = "/")
+#    unsampled_indices_path <-
+#      paste(save_dir, "unsampled_indices.txt", sep = "/")
 
-    unsampled_indices <-
-      as.numeric(readLines(con = unsampled_indices_path))
+#    unsampled_indices <-
+#      as.numeric(readLines(con = unsampled_indices_path))
 
-    output_table_path <- paste(save_dir, "output_table.txt", sep = "/")
-    output_table <- read.csv(output_table_path, colClasses = "character")
+#    output_table_path <- paste(save_dir, "output_table.txt", sep = "/")
+#    output_table <- read.csv(output_table_path, colClasses = "character")
 
 
-    blast_seeds_m_path <- paste(save_dir, "blast_seeds_passed_filter.txt", sep = "/")
-    blast_seeds_m <- read.csv(blast_seeds_m_path, colClasses = "character")
+#    blast_seeds_m_path <- paste(save_dir, "blast_seeds_passed_filter.txt", sep = "/")
+#    blast_seeds_m <- read.csv(blast_seeds_m_path, colClasses = "character")
 
-  }
+#  }
 
   while (length(unsampled_indices) > 0) {
 
-
-  ######### testing
-    message("2")
-
-    ######### testing
-      message("4.5")
-    save_state(save_dir, output_table, unsampled_indices, too_many_ns,
-                  blastdbcmd_failed, num_rounds, blast_seeds_m)
-
-    ######### testing
-      message("4.7")
-
-      rm(output_table)
-      rm(unsampled_indices)
-      rm(too_many_ns)
-      rm(blastdbcmd_failed)
-      rm(blast_seeds_m)
 
     ######### testing
       message("5")
@@ -285,6 +268,22 @@ blast_datatable <- function(blast_seeds, save_dir, db, accession_taxa_path,
 
   ######### testing
     message("9")
+
+
+
+  ######### testing
+        message("4.5")
+      save_state(save_dir, output_table, unsampled_indices, too_many_ns,
+                    blastdbcmd_failed, num_rounds, blast_seeds_m)
+
+  ######### testing
+        message("4.7")
+
+        rm(output_table)
+        rm(unsampled_indices)
+        rm(too_many_ns)
+        rm(blastdbcmd_failed)
+        rm(blast_seeds_m)
 
   }
 
