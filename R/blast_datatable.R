9e4c0b407a8c9f916a9297c28379e4e68e4d1cb0
limey-bean <eecurd@g.ucla.edu>
2022-09-27 20:16:28 -0400

    fix pick file append problem

--- R/blast_datatable.R
+++ R/blast_datatable.R
@@ -80,33 +80,9 @@ blast_datatable <- function(blast_seeds, save_dir, db, accession_taxa_path,
   unsampled_indices <- seq_along(blast_seeds_m$accession)
 
   # Pick up where it left off
-  # This could be improved in a bunch of ways tbh
-  # Most simply, this does the "same thing" 5 times. function
-  if (file.exists(paste(save_dir, "unsampled_indices.txt", sep = "/"))) {
-    rounds_path <- paste(save_dir, "num_rounds.txt", sep = "/")
-    num_rounds <- as.numeric(readLines(con = rounds_path))
-
-    ns_path <- paste(save_dir, "too_many_ns.txt", sep = "/")
-    too_many_ns <- as.numeric(readLines(con = ns_path))
-
-    blastdbcmd_failed_path <- paste(save_dir, "blastdbcmd_failed.txt", sep = "/")
-    blastdbcmd_failed <- as.numeric(readLines(con = blastdbcmd_failed_path))
-
-    unsampled_indices_path <-
-      paste(save_dir, "unsampled_indices.txt", sep = "/")
-    unsampled_indices <-
-      as.numeric(readLines(con = unsampled_indices_path))
-
-    output_table_path <- paste(save_dir, "output_table.txt", sep = "/")
-    output_table <- read.csv(output_table_path, colClasses = "character")
-
-    output_table_path <- paste(save_dir, "output_table.txt", sep = "/")
-    output_table <- read.csv(output_table_path, colClasses = "character")
-
-    blast_seeds_m_path <- paste(save_dir, "blast_seeds_passed_filter.txt", sep = "/")
-    blast_seeds_m <- read.csv(blast_seeds_m_path, colClasses = "character")
-
-  }
+  pickup_output_for_blasting(save_dir, output_table,
+                unsampled_indices, too_many_ns, blastdbcmd_failed, num_rounds,
+                blast_seeds_m)
 
 
   while (length(unsampled_indices) > 0) {
@@ -155,6 +131,11 @@ blast_datatable <- function(blast_seeds, save_dir, db, accession_taxa_path,
       if (length(sample_indices) <= max_to_blast) {
       #testing
       message("max to blast or fewer")
+
+      pickup_output_for_blasting(save_dir, output_table,
+                    unsampled_indices, too_many_ns, blastdbcmd_failed, num_rounds,
+                    blast_seeds_m)
+
       run_blastdbcmd_blastn_and_aggregate_resuts(sample_indices, save_dir,
             blast_seeds_m, db, ncbi_bin = NULL, too_many_ns, db_dir,
             blastdbcmd_failed, unsampled_indices, output_table, wildcards,
@@ -168,10 +149,15 @@ blast_datatable <- function(blast_seeds, save_dir, db, accession_taxa_path,
         # take chunks of the sample indices that are equivalent to max_to_blast
         subset <- head(sample_indices, max_to_blast)
 
+        pickup_output_for_blasting(save_dir, output_table,
+                      unsampled_indices, too_many_ns, blastdbcmd_failed, num_rounds,
+                      blast_seeds_m)
+
         run_blastdbcmd_blastn_and_aggregate_resuts(subset, save_dir,
               blast_seeds_m, db, ncbi_bin = NULL, too_many_ns, db_dir,
               blastdbcmd_failed, unsampled_indices, output_table, wildcards,
               num_rounds)
+
         # update sample indices
         sample_indices <- sample_indices[!(sample_indices %in% subset)]
       }
---
+++ R/dedup_sequences.R
@@ -0,0 +1,29 @@
+#' function to deduplicate results based on identical sequences
+#'
+#' @param output_dir the path to the output directory
+#' @param input_file the path to the input file
+#' @param metabarcode name of metabarcode
+
+#' @return NULL
+#' @export
+
+#library(data.table)
+#library(dplyr)
+
+#metabarcode <- "12S_MiFish"
+#output_dir <- "~/Dropbox/CRUX_2.0/12S_MiFish_3-10000/rcrux_blast_output/"
+#input_file"~/Dropbox/CRUX_2.0/12S_MiFish_3-10000/rcrux_blast_output/summary.csv"
+
+summary <- read.csv(input_file)
+
+# get smaller df to work with
+summary <- summary %>% select(accession, amplicon_length, sequence, phylum, class, order, family, genus, species)
+
+#remove hyphens
+summary <-  dplyr::mutate(summary, sequence = gsub("-", "", sequence))
+
+# ugly but effective
+phy_sum <- summary %>% group_by(sequence) %>% summarize(accession = paste0(accession, collapse = ", "), amplicon_length = paste0(unique(amplicon_length), collapse = ", "), phylum = paste0(unique(phylum), collapse = ", "), class = paste0(unique(class), collapse = ", "), order = paste0(unique(order), collapse = ", "), family = paste0(unique(family), collapse = ", "), genus = paste0(unique(genus), collapse = ", "),   species = paste0(unique(species), collapse = ", "))
+
+phy_sum_path <- paste0(output_dir, "/", metabarcode, "_phy_sum.txt")
+write.table(phy_sum, file = phy_sum_path, row.names = FALSE, col.names=FALSE, sep = "\t")
