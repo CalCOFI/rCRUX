c6bb7e3140539f0e84ded5acf3d6d7f3c3cf71db
limey-bean <eecurd@g.ucla.edu>
2022-09-28 09:56:29 -0400

    save state

--- R/blast_datatable.R
+++ R/blast_datatable.R
@@ -212,6 +212,35 @@ blast_datatable <- function(blast_seeds, save_dir, db, accession_taxa_path,
 
     while (length(sample_indices) > 0 ){
 
+    ######### testing
+      message("1.6.7")
+
+      # Pick up where it left off
+      if (file.exists(paste(save_dir, "unsampled_indices.txt", sep = "/"))) {
+
+        rounds_path <- paste(save_dir, "num_rounds.txt", sep = "/")
+        num_rounds <- as.numeric(readLines(con = rounds_path))
+
+        ns_path <- paste(save_dir, "too_many_ns.txt", sep = "/")
+        too_many_ns <- as.numeric(readLines(con = ns_path))
+
+        blastdbcmd_failed_path <- paste(save_dir, "blastdbcmd_failed.txt", sep = "/")
+        blastdbcmd_failed <- as.numeric(readLines(con = blastdbcmd_failed_path))
+
+        unsampled_indices_path <-
+          paste(save_dir, "unsampled_indices.txt", sep = "/")
+
+        unsampled_indices <-
+          as.numeric(readLines(con = unsampled_indices_path))
+
+        output_table_path <- paste(save_dir, "output_table.txt", sep = "/")
+        output_table <- read.csv(output_table_path, colClasses = "character")
+
+
+        blast_seeds_m_path <- paste(save_dir, "blast_seeds_passed_filter.txt", sep = "/")
+        blast_seeds_m <- read.csv(blast_seeds_m_path, colClasses = "character")
+
+      }
 
       if (length(sample_indices) <= max_to_blast) {
 
--- R/run_blastdbcmd_blastn_and_aggregate_resuts.R
+++ R/run_blastdbcmd_blastn_and_aggregate_resuts.R
@@ -98,16 +98,56 @@ run_blastdbcmd_blastn_and_aggregate_resuts <- function(sample_indices = sample_i
       dplyr::filter(!(duplicated(accession)))
       output_table <- dplyr::ungroup(output_table)
 
+      # save the state of the blast does not work here...
 
    }
-
-   # save the state of the blast
-   save_state(save_dir, output_table, unsampled_indices, too_many_ns,
-              blastdbcmd_failed, num_rounds, blast_seeds_m)
 
    # report number of total unique blast hits
 
    message(nrow(output_table), " unique blast hits after this round.")
+
+   ######### testing
+     message("4.5")
+   save_state(save_dir, output_table, unsampled_indices, too_many_ns,
+                 blastdbcmd_failed, num_rounds, blast_seeds_m)
+
+   ######### testing
+     message("4.7")
+
+     rm(output_table)
+     rm(unsampled_indices)
+     rm(too_many_ns)
+     rm(blastdbcmd_failed)
+     rm(num_rounds)
+     rm(blast_seeds_m)
+
+   ######### testing
+     message("5")
+
+     if (file.exists(paste(save_dir, "unsampled_indices.txt", sep = "/"))) {
+
+       rounds_path <- paste(save_dir, "num_rounds.txt", sep = "/")
+       num_rounds <- as.numeric(readLines(con = rounds_path))
+
+       ns_path <- paste(save_dir, "too_many_ns.txt", sep = "/")
+       too_many_ns <- as.numeric(readLines(con = ns_path))
+
+       blastdbcmd_failed_path <- paste(save_dir, "blastdbcmd_failed.txt", sep = "/")
+       blastdbcmd_failed <- as.numeric(readLines(con = blastdbcmd_failed_path))
+
+       unsampled_indices_path <-
+         paste(save_dir, "unsampled_indices.txt", sep = "/")
+       unsampled_indices <-
+         as.numeric(readLines(con = unsampled_indices_path))
+
+       output_table_path <- paste(save_dir, "output_table.txt", sep = "/")
+       output_table <- read.csv(output_table_path, colClasses = "character")
+
+
+       blast_seeds_m_path <- paste(save_dir, "blast_seeds_passed_filter.txt", sep = "/")
+       blast_seeds_m <- read.csv(blast_seeds_m_path, colClasses = "character")
+
+     }
 
 
 }
