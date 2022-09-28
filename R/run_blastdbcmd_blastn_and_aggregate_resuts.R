50eea78a35fdd20be6f7c15025b27b2626c568e7
limey-bean <eecurd@g.ucla.edu>
2022-09-27 22:17:16 -0400

    Update run_blastdbcmd_blastn_and_aggregate_resuts.R

--- R/run_blastdbcmd_blastn_and_aggregate_resuts.R
+++ R/run_blastdbcmd_blastn_and_aggregate_resuts.R
@@ -108,6 +108,11 @@ run_blastdbcmd_blastn_and_aggregate_resuts <- function(sample_indices = sample_i
 
    message(nrow(output_table), " unique blast hits after this round.")
 
-
+   rm(output_table)
+   rm(unsampled_indices)
+   rm(too_many_ns)
+   rm(blastdbcmd_failed)
+   rm(num_rounds)
+   rm(blast_seeds_m)
 
 }
