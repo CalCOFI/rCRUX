d2774ed460576c08519d57fe29684d1eb9e8aa88
limey-bean <eecurd@g.ucla.edu>
2022-10-03 10:29:05 -0400

    Update blast_datatable.R

--- R/blast_datatable.R
+++ R/blast_datatable.R
@@ -200,7 +200,7 @@ blast_datatable <- function(blast_seeds, save_dir, db, accession_taxa_path,
 
       }
 
-      if (length(sample_indices) <= max_to_blast && length(unsampled_indices) <= max_to_blast) {
+      if (length(sample_indices) <= max_to_blast && (length(unsampled_indices) + length(sample_indices)) <= max_to_blast) {
 
         run_blastdbcmd_blastn_and_aggregate_resuts(unsampled_indices, save_dir,
           blast_seeds_m, db, ncbi_bin = NULL, too_many_ns, db_dir,
@@ -210,7 +210,7 @@ blast_datatable <- function(blast_seeds, save_dir, db, accession_taxa_path,
           end <- TRUE
 
           break
-      
+
 
       break
 
