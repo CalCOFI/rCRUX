64c898a307e007b6fbafe6f98e359d384ed3a60a
limey-bean <eecurd@g.ucla.edu>
2022-10-03 11:18:41 -0400

    Update blast_datatable.R

--- R/blast_datatable.R
+++ R/blast_datatable.R
@@ -206,7 +206,7 @@ blast_datatable <- function(blast_seeds, save_dir, db, accession_taxa_path,
 
       if (length(sample_indices) <= max_to_blast || move == TRUE) {
 
-        run_blastdbcmd_blastn_and_aggregate_resuts(c(unsampled_indices, sample_indices), save_dir,
+        run_blastdbcmd_blastn_and_aggregate_resuts(unsampled_indices, save_dir,
           blast_seeds_m, db, ncbi_bin = NULL, too_many_ns, db_dir,
           blastdbcmd_failed, unsampled_indices, output_table, wildcards,
           num_rounds)
