fb9729d1c29b91804891b0075c1a1c7ac1476abc
limey-bean <eecurd@g.ucla.edu>
2022-10-04 22:43:55 -0400

    Update dedup_sequences.R

--- R/dedup_sequences.R
+++ R/dedup_sequences.R
@@ -1,29 +1,72 @@
 #' function to deduplicate results based on identical sequences
 #'
 #' @param output_dir the path to the output directory
-#' @param input_file the path to the input file
-#' @param metabarcode name of metabarcode
+#' @param summary_path the path to the input file
 
 #' @return NULL
 #' @export
 
-#library(data.table)
-#library(dplyr)
+# function to summarize the rcrux summary data by identical sequence and identify / collapse reads with multiple assignments for a given taxonomic rank.
+# "We ain't too pretty, we ain't too proud" - Billy Joel, "Only the good die young"
 
-#metabarcode <- "12S_MiFish"
-#output_dir <- "~/Dropbox/CRUX_2.0/12S_MiFish_3-10000/rcrux_blast_output/"
-#input_file"~/Dropbox/CRUX_2.0/12S_MiFish_3-10000/rcrux_blast_output/summary.csv"
 
-#summary <- read.csv(input_file)
+dedup <- function(output_dir, summary_path, rank = c("phylum", "class", "order", "family", "genus", "species"), remove_NA = TRUE ) {
 
-# get smaller df to work with
-#summary <- summary %>% select(accession, amplicon_length, sequence, phylum, class, order, family, genus, species)
+  # read rcrux blast summary.csv
+  summary <- read.csv(summary_path)
 
-#remove hyphens
-#summary <-  dplyr::mutate(summary, sequence = gsub("-", "", sequence))
+  # get relevant df to work with
+  summary <- dplyr::select(summary, accession, amplicon_length, sequence, phylum, class, order, family, genus, species)
 
-# ugly but effective
-#phy_sum <- summary %>% group_by(sequence) %>% summarize(accession = paste0(accession, collapse = ", "), amplicon_length = paste0(unique(amplicon_length), collapse = ", "), phylum = paste0(unique(phylum), collapse = ", "), class = paste0(unique(class), collapse = ", "), order = paste0(unique(order), collapse = ", "), family = paste0(unique(family), collapse = ", "), genus = paste0(unique(genus), collapse = ", "),   species = paste0(unique(species), collapse = ", "))
+  #remove hyphens from sequence
+  summary <-  dplyr::mutate(summary, sequence = gsub("-", "", sequence))
 
-#phy_sum_path <- paste0(output_dir, "/", metabarcode, "_phy_sum.txt")
-#write.table(phy_sum, file = phy_sum_path, row.names = FALSE, col.names=FALSE, sep = "\t")
+  # merge accessions and ranks for identical sequence
+  phy_sum <- summary %>% group_by(sequence) %>% summarize(accession = paste0(accession, collapse = ", "), amplicon_length = paste0(unique(amplicon_length), collapse = ", "), phylum = paste0(unique(phylum), collapse = ", "), class = paste0(unique(class), collapse = ", "), order = paste0(unique(order), collapse = ", "), family = paste0(unique(family), collapse = ", "), genus = paste0(unique(genus), collapse = ", "),   species = paste0(unique(species), collapse = ", "))
+
+  #count number of accessions make new column
+  phy_sum <- dplyr::mutate(phy_sum, num_of_accessions = (stringr::str_count(accession, ",") + 1 ))
+
+  # remove , NA from ranks - they are most likely due to env seq or issues with sequence submission
+
+  if (remove_NA == TRUE){
+    for (r in rank){
+      phy_sum <- dplyr::mutate(phy_sum, !! r := sub(", NA", "", !! sym(r)))
+    }
+
+  }
+
+
+  #Identify rows with multiple ids
+  # subset dups:
+  sub_dups <- dplyr::filter(phy_sum, grepl(', ', phylum) | grepl(', ', class) | grepl(', ', order) | grepl(', ', family) | grepl(', ', genus) | grepl(', ', species))
+
+
+  #remove single taxonomy sequence
+  clean_tax <- dplyr::setdiff(phy_sum, sub_dups)
+
+
+  # change rank to NA if multiple names
+  sub_dup_to_NA <- sub_dups
+
+  for (r in rank){
+    sub_dup_to_NA <- dplyr::mutate(sub_dup_to_NA, !! r := sub(".*, .*", "NA", !! sym(r)))
+  }
+
+
+  # write output
+
+  write.csv(sub_dups,
+            file = paste(output_dir, "References_with_multiple_taxonomic_ranks.csv", sep = "/"),
+            row.names = FALSE)
+
+  write.csv(sub_dup_to_NA,
+            file = paste(output_dir, "References_with_NA_instead_of_multiple_taxonomic_ranks.csv", sep = "/"),
+            row.names = FALSE)
+
+
+  write.csv(clean_tax,
+            file = paste(output_dir, "References_with_unique_taxonomic_ranks.csv", sep = "/"),
+            row.names = FALSE)
+
+}
