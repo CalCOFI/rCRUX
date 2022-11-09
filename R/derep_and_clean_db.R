0736559342d2b577eeb2e8be9bb59e149aa95d11
limey-bean <eecurd@g.ucla.edu>
2022-11-09 11:12:24 -0500

    Update derep_and_clean_db.R

--- R/derep_and_clean_db.R
+++ R/derep_and_clean_db.R
@@ -13,7 +13,7 @@
 derep_and_clean_db <- function(output_directory_path, summary_path, metabarcode_name) {
 
   out <- paste0(output_directory_path, "/derep_and_clean_db/")
-  suppress.warnings(dir.create(out))
+  suppressWarnings(dir.create(out))
 
 
   # read rcrux blast summary.csv
