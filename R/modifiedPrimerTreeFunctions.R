53c1bd87d9fd64fd7394f15e2110f9844385c0c2
Emily Curd <eecurd@g.ucla.edu>
2022-11-18 12:40:06 -0500

    Update modifiedPrimerTreeFunctions.R

--- R/modifiedPrimerTreeFunctions.R
+++ R/modifiedPrimerTreeFunctions.R
@@ -162,7 +162,7 @@ POST_retry = retry(POST)
 #' @noRd
 parse_primer_hits = function(response){
   content = parsable_html(response)
-  rbind.fill(xpathApply(content, '//pre', parse_pre))
+  dplyr::rbind.fill(xpathApply(content, '//pre', parse_pre))
 }
 parse_a = function(a){
   #links like entrez/viewer.fcgi?db=nucleotide&id=452085006
