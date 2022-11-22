2eb27d94b1b09416027dd7bb2952df0495e9702c
limey-bean <eecurd@g.ucla.edu>
2022-11-21 22:11:30 -0500

    bugs

--- R/run_blastdbcmd.R
+++ R/run_blastdbcmd.R
@@ -51,7 +51,6 @@ run_blastdbcmd <- function(query_row, db, ncbi_bin = NULL) {
     }
     else {
         blastdbcmd_path <- paste0(ncbi_bin, "blastdbcmd")
-        print(blastdbcmd_path)
         fasta <- system2(command= blastdbcmd_path, args = c("-db", db,
                                                 "-dbtype", "nucl",
                                                 "-entry", accession,
--- R/run_blastdbcmd_blastn_and_aggregate_resuts.R
+++ R/run_blastdbcmd_blastn_and_aggregate_resuts.R
@@ -46,7 +46,7 @@ run_blastdbcmd_blastn_and_aggregate_resuts <- function(sample_indices = sample_i
         unsampled_indices, output_table, wildcards, num_rounds, ...) {
 
 
-
+  print(ncbi_bin)
   # run blastdbcmd on each
   # sort results into appropriate buckets
   aggregate_fasta <- NULL
