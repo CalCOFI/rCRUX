5013332fb82ba4d8a2fd5bda03f9188e43ba9cd7
limey-bean <eecurd@g.ucla.edu>
2023-01-12 12:19:53 -0500

    Update blast_datatable.R

--- R/blast_datatable.R
+++ R/blast_datatable.R
@@ -316,56 +316,58 @@ blast_datatable <- function(blast_seeds, save_dir, blast_db_path, accession_taxa
   # Note - we are not identifying or unpacking the representative accessions
   # that report a single taxid
 
+  # !!!! Buggy!
+
     #Identify rows with multiple ids and filter into new dataframe
-    multi_taxids <- dplyr::filter(output_table, grepl(';', BLAST_db_taxids))
+    #multi_taxids <- dplyr::filter(output_table, grepl(';', BLAST_db_taxids))
 
     #remove multitaxids from output table
-    clean_tax <- dplyr::setdiff(output_table, multi_taxids)
+    #clean_tax <- dplyr::setdiff(output_table, multi_taxids)
 
     #make a list of multi_taxid accessions for blastdbcmd
-    accession <- paste0(multi_taxids$accession, collapse=",")
+    #accession <- paste0(multi_taxids$accession, collapse=",")
 
-    message("here")
+
 
     # send accessions through blastdbcmd
-    expand_multi_taxids_output <- system2("blastdbcmd", args = c("-db", blast_db_path,
+    #expand_multi_taxids_output <- system2("blastdbcmd", args = c("-db", blast_db_path,
                                                              "-dbtype", "nucl",
                                                              "-entry", accession,
                                                              "-outfmt", "'%a %T'"),
                                                    stdout = TRUE, stderr = FALSE)
 
     #make df to store fixed multi taxid info
-    accession_df <- dplyr::select(multi_taxids, accession)
+    #accession_df <- dplyr::select(multi_taxids, accession)
 
     # make df for blastdbcmd output
-    column_names <-  c("accession",
+    #column_names <-  c("accession",
                    "BLAST_db_taxids")
 
-    accession_output_table <- tibble::as_tibble(expand_multi_taxids_output)
-    accession_output_table <-  tidyr::separate(accession_output_table, col = value, into = column_names,
+    #accession_output_table <- tibble::as_tibble(expand_multi_taxids_output)
+    #accession_output_table <-  tidyr::separate(accession_output_table, col = value, into = column_names,
                                  sep = " ")
 
     # add row_id for sorting later
-    accession_output_table <- dplyr::mutate(accession_output_table, row_id= dplyr::row_number())
+    #accession_output_table <- dplyr::mutate(accession_output_table, row_id= dplyr::row_number())
 
     # left join accessions that had mutli_taxids  with the blastcmd output - there wil be NA's so sort by row number to keep related blastdbcm output together - accessions that are identical
-    accession_df <- dplyr::full_join(accession_df, accession_output_table,  by = "accession", keep = TRUE)
-    accession_df <- dplyr::arrange(accession_df, row_id)
+    #accession_df <- dplyr::full_join(accession_df, accession_output_table,  by = "accession", keep = TRUE)
+    #accession_df <- dplyr::arrange(accession_df, row_id)
 
     # fill NA's in columns with the initial accession value
-    accession_df <- zoo::na.locf(zoo::na.locf(accession_df), fromLast=TRUE)
+    #accession_df <- zoo::na.locf(zoo::na.locf(accession_df), fromLast=TRUE)
 
     # left join original multi_taxid table with the new expanded table -  fleshes out the info
-    accession_df <- dplyr::left_join(accession_df, multi_taxids, by = c("accession.x" = "accession"))
+    #accession_df <- dplyr::left_join(accession_df, multi_taxids, by = c("accession.x" = "accession"))
 
     # remove unnecessary columns
-    accession_df <- dplyr::select(accession_df, -c(accession.x, BLAST_db_taxids.y, row_id))
+    #accession_df <- dplyr::select(accession_df, -c(accession.x, BLAST_db_taxids.y, row_id))
 
     # change name of columns for concatonating with the clean data
-    accession_df <- dplyr::rename(accession_df, accession = accession.y, BLAST_db_taxids=BLAST_db_taxids.x)
+    #accession_df <- dplyr::rename(accession_df, accession = accession.y, BLAST_db_taxids=BLAST_db_taxids.x)
 
     # add the expanded blastdbcmd output with the single taxid table.
-    output_table <- rbind(accession_df, clean_tax)
+    #output_table <- rbind(accession_df, clean_tax)
 
 
     output_table_taxonomy <-
