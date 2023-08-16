e62e7e5c88ffcee02a5eb65882e44993b3c92161
Zack Gold <zack.j.gold@gmail.com>
2023-08-16 08:47:50 -0700

    revert blast_datatable.R

--- R/blast_datatable.R
+++ R/blast_datatable.R
@@ -98,13 +98,11 @@
 
 
 blast_datatable <- function(blast_seeds, save_dir, blast_db_path, accession_taxa_sql_path,
-                            force_db = FALSE,
+                            ncbi_bin = NULL, force_db = FALSE,
                             sample_size = 1, wildcards = "NNNNNNNNNNNN", rank = 'genus', max_to_blast = 1000, random_seed = NULL, ...) {
-    dots <- list(...)
-    check_blast_plus_installation(ncbi_bin = if('ncbi_bin' %in% names(dots)) dots$ncbi_bin else NULL)
-
-    check_blast_db(blast_db_path, ncbi_bin = if('ncbi_bin' %in% names(dots)) dots$ncbi_bin else NULL)
- 
+  
+  check_blast_plus_installation(ncbi_bin = if('ncbi_bin' %in% names(list(...))) ncbi_bin else NULL)
+  check_blast_db(blast_db_path)
   if (!file.exists(accession_taxa_sql_path)) {
     stop("accession_taxa_sql_path does not exist.\n",
          "The path to the taxonomizr SQL file cannot be found. ",
